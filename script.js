5fed2638eda9d52e56b15f4f77d71c887e44faf7
Maxime Orione <maxpower98@hotmail.com>
2017-03-29 15:38:51 +0200

    Get All & Get By ID Finished

--- script.js
+++ script.js
@@ -29,3 +29,21 @@ function getEventsByID(id) {
     console.log(xhr.status);
     console.log(xhr.statusText);
 }
+
+function getEventsBetweenDates(date1,date2) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("output").innerHTML = this.responseText;
+        }
+    };
+
+    xhr.open("GET", "geteventbetweendates.php?from=+"+date1+"\"&until=+"+date2, false);
+    xhr.send();
+
+    console.log("Date1: "+date1);
+
+    console.log(xhr.status);
+    console.log(xhr.statusText);
+}
