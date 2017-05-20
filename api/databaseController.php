a47996ee8469d394a377fdc42f4643487d4ee27b
neelnilis <neel.nilis@student.pxl.be>
2017-05-20 10:54:01 +0200

    bestanden gekopieerd naar github

---
+++ api/databaseController.php
@@ -0,0 +1,229 @@
+<?php
+
+class DBController
+{
+    function getAll() {
+        try {
+            // Open connection to DB:
+            $pdo = new PDO("mysql:host=localhost;dbname=monkey",
+                'root', 'user' );
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // Create array for each result:
+            $i = 0;
+            $events = array();
+            foreach($pdo->query("SELECT * FROM events") as $row) {
+                // Create class:
+                $events[$i] = new StdClass();
+                $events[$i]->id = $row['ID'];
+                $events[$i]->name = $row['Name'];
+                $events[$i]->personid = $row['PersonID'];
+                $events[$i]->startdate = $row['StartDate'];
+                $events[$i]->enddate = $row['EndDate'];
+                $i++;
+            }
+        }
+        catch ( PDOException $e ) {
+            print 'Exception!: ' . $e->getMessage();
+        }
+        $pdo = null; // Close connection
+
+        // Send results:
+        http_response_code(200);
+        header('Content-Type: application/json');
+        echo json_encode($events, JSON_PRETTY_PRINT);
+    }
+
+    function getByID($id)
+    {
+        // Check for empty id:
+        if ($id == null) {
+            http_response_code(406);
+        }
+        else {
+            try {
+                // Open connection to DB:
+                $pdo = new PDO("mysql:host=localhost;dbname=monkey",
+                    'root', 'user');
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                // Create array for each result:
+                $i = 0;
+                $events = array();
+                foreach ($pdo->query("SELECT * FROM events WHERE ID=" . $id) as $row) {
+                    // Create class:
+                    $events[$i] = new StdClass();
+                    $events[$i]->id = $row['ID'];
+                    $events[$i]->name = $row['Name'];
+                    $events[$i]->personid = $row['PersonID'];
+                    $events[$i]->startdate = $row['StartDate'];
+                    $events[$i]->enddate = $row['EndDate'];
+                    $i++;
+                }
+            } catch (PDOException $e) {
+                print 'Exception!: ' . $e->getMessage();
+            }
+            $pdo = null; // Close connection
+
+            // Send results:
+            http_response_code(200);
+            header('Content-Type: application/json');
+            echo json_encode($events, JSON_PRETTY_PRINT);
+        }
+    }
+
+    function getByPersonID($id)
+    {
+        // Check for empty id:
+        if ($id == null) {
+            http_response_code(406);
+        }
+        else {
+            try {
+                // Open connection to DB:
+                $pdo = new PDO("mysql:host=localhost;dbname=monkey",
+                    'root', 'user');
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                // Create array for each result:
+                $i = 0;
+                $events = array();
+                foreach ($pdo->query("SELECT * FROM events WHERE PersonID=" . $id) as $row) {
+                    // Create class:
+                    $events[$i] = new StdClass();
+                    $events[$i]->id = $row['ID'];
+                    $events[$i]->name = $row['Name'];
+                    $events[$i]->personid = $row['PersonID'];
+                    $events[$i]->startdate = $row['StartDate'];
+                    $events[$i]->enddate = $row['EndDate'];
+                    $i++;
+                }
+            } catch (PDOException $e) {
+                print 'Exception!: ' . $e->getMessage();
+            }
+            $pdo = null; // Close connection
+
+            // Send results:
+            http_response_code(200);
+            header('Content-Type: application/json');
+            echo json_encode($events, JSON_PRETTY_PRINT);
+        }
+    }
+
+    function getBetweenDates($from,$until) {
+        // Check for empty from & until:
+        if ($from == null || $until == null) {
+            http_response_code(406);
+        }
+        else {
+            try {
+                // Open connection to DB:
+                $pdo = new PDO("mysql:host=localhost;dbname=monkey",
+                    'root', 'user');
+                $pdo->setAttribute(PDO::ATTR_ERRMODE,
+                    PDO::ERRMODE_EXCEPTION);
+
+                // Create array for each result:
+                $i = 0;
+                $events = array();
+                foreach ($pdo->query("SELECT * FROM events WHERE StartDate > '" . $from . "' AND EndDate < '" . $until . "'") as $row) {
+                    // Create class:
+                    $events[$i] = new StdClass();
+                    $events[$i]->id = $row['ID'];
+                    $events[$i]->name = $row['Name'];
+                    $events[$i]->personid = $row['PersonID'];
+                    $events[$i]->startdate = $row['StartDate'];
+                    $events[$i]->enddate = $row['EndDate'];
+                    $i++;
+                }
+            } catch (PDOException $e) {
+                print 'Exception!: ' . $e->getMessage();
+            }
+            $pdo = null; // Close connection
+
+            // Send results:
+            http_response_code(200);
+            header('Content-Type: application/json');
+            echo json_encode($events, JSON_PRETTY_PRINT);
+        }
+    }
+
+    function getByPersonIDAndDates($id,$from,$until) {
+        // Check for empty id, from & until:
+        if ($id == null || $from == null || $until == null) {
+            http_response_code(406);
+        }
+        else {
+            try {
+                // Open connection to DB:
+                $pdo = new PDO("mysql:host=localhost;dbname=monkey",
+                    'root', 'user');
+                $pdo->setAttribute(PDO::ATTR_ERRMODE,
+                    PDO::ERRMODE_EXCEPTION);
+
+                // Create array for each result:
+                $i = 0;
+                $events = array();
+                foreach ($pdo->query("SELECT * FROM events WHERE StartDate > '" . $from . "' AND EndDate < '" . $until . "' AND PersonID = ". $id) as $row) {
+                    // Create class:
+                    $events[$i] = new StdClass();
+                    $events[$i]->id = $row['ID'];
+                    $events[$i]->name = $row['Name'];
+                    $events[$i]->personid = $row['PersonID'];
+                    $events[$i]->startdate = $row['StartDate'];
+                    $events[$i]->enddate = $row['EndDate'];
+                    $i++;
+                }
+            } catch (PDOException $e) {
+                print 'Exception!: ' . $e->getMessage();
+            }
+            $pdo = null; // Close connection
+
+            // Send results:
+            http_response_code(200);
+            header('Content-Type: application/json');
+            echo json_encode($events, JSON_PRETTY_PRINT);
+        }
+    }
+
+    function postEvent($req) {
+        // Decode incoming request body to JSON
+        $json = json_decode($req, true);
+
+        // Check for empty name, personid, startdate & enddate:
+        if ($json['name'] == null ||
+            $json['personid'] == null ||
+            $json['startdate'] == null ||
+            $json['enddate'] == null) {
+            http_response_code(406);
+        }
+        else {
+            try {
+                // Open connection to DB:
+                $pdo = new PDO("mysql:host=localhost;dbname=monkey",
+                    'root', 'user');
+                $pdo->setAttribute(PDO::ATTR_ERRMODE,
+                    PDO::ERRMODE_EXCEPTION);
+
+                // Create prepared INSERT statement
+                $stmt = $pdo->prepare("INSERT INTO events(Name, PersonID, StartDate, EndDate) VALUES (:name, :personid, :startdate, :enddate)");
+
+                // Bind parameters:
+                $stmt->bindParam(':name', $json['name']);
+                $stmt->bindParam(':personid', $json['personid']);
+                $stmt->bindParam(':startdate', $json['startdate']);
+                $stmt->bindParam(':enddate', $json['enddate']);
+
+                // Execute INSERT
+                $stmt->execute();
+            } catch (PDOException $e) {
+                print 'Exception!: ' . $e->getMessage();
+            }
+            $pdo = null; // Close connection
+
+            // Send OK
+            http_response_code(200);
+        }
+    }
+}
+?>
---
+++ script.js
@@ -0,0 +1,112 @@
+/**
+ * Created by 11500683 on 17/05/2017.
+ */
+function getAllEvents() {
+    var xhr = new XMLHttpRequest();
+
+    // Set output field with responsetext when GET is done:
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("output").innerHTML = this.responseText;
+        }
+    };
+
+    // Open and send GET request:
+    xhr.open("GET", "api/events/", false);
+    xhr.send();
+}
+
+function getEventsByID(id) {
+    var xhr = new XMLHttpRequest();
+
+    // Set output field with responsetext when GET is done:
+    xhr.onreadystatechange = function() {
+        // Catch error:
+        if (this.readyState == 4 && this.status == 406) {
+            alert("ID is not set");
+        }
+        else if (this.readyState === 4 && this.status === 200) {
+            document.getElementById("output").innerHTML = this.responseText;
+        }
+    };
+
+    // Open and send GET request:
+    xhr.open("GET", "api/events/"+id, false);
+    xhr.send();
+}
+
+function getEventsByPerson(id) {
+    var xhr = new XMLHttpRequest();
+
+    // Set output field with responsetext when GET is done:
+    xhr.onreadystatechange = function() {
+        // Catch error:
+        if (this.readyState == 4 && this.status == 406) {
+            alert("ID is not set");
+        }
+        else if (this.readyState === 4 && this.status === 200) {
+            document.getElementById("output").innerHTML = this.responseText;
+        }
+    };
+
+    // Open and send GET request:
+    xhr.open("GET", "api/events/person/"+id, false);
+    xhr.send();
+}
+
+function getEventsBetweenDates(date1, date2) {
+    var xhr = new XMLHttpRequest();
+
+    // Set output field with responsetext when GET is done:
+    xhr.onreadystatechange = function() {
+        // Catch error:
+        if (this.readyState == 4 && this.status == 406) {
+            alert("Not all dates are set");
+        }
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("output").innerHTML = this.responseText;
+        }
+    };
+
+    // Open and send GET request:
+    xhr.open("GET", "api/events/byDate/?from="+date1+"&until="+date2, false);
+    xhr.send();
+}
+
+function getByPersonAndBetweenDates(person, date1, date2) {
+    var xhr = new XMLHttpRequest();
+
+    // Set output field with responsetext when GET is done:
+    xhr.onreadystatechange = function() {
+        // Catch error:
+        if (this.readyState == 4 && this.status == 406) {
+            alert("Not all data is set");
+        }
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("output").innerHTML = this.responseText;
+        }
+    };
+
+    // Open and send GET request:
+    xhr.open("GET", "api/events/person/"+person+"/byDate/?from="+date1+"&until="+date2, false);
+    xhr.send();
+}
+
+function postEvent(name, person, date1, date2) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        // Catch error:
+        if (this.readyState == 4 && this.status == 406) {
+            alert("Not all data is set");
+        }
+        else if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("output").innerHTML = this.responseText;
+            alert(name + " toegevoegd!");
+        }
+    }
+
+    xhr.open("POST", "api/events/", true); // Open POST request
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8"); // Set request body
+    xhr.send(JSON.stringify({name: name, personid: person, startdate: date1, enddate: date2})); // Send POST request
+}
